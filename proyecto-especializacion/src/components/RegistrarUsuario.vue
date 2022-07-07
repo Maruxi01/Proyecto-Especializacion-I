df2b701e540eb9552536a3fad9e04b1c83b3ca6a
Tomas Valdes <tomasvicenteignacio@gmail.com>
2022-07-06 21:52:42 -0400

    Se crea formulario para registrarse

--- proyecto-especializacion/src/components/RegistrarUsuario.vue
+++ proyecto-especializacion/src/components/RegistrarUsuario.vue
@@ -1,6 +1,44 @@
 <!-- eslint-disable  -->
 <template>
-  <div class="hello">
+  <div>
+    <div class="container-md justify-content-center">
+      <div class="row justify-content-center" id="register">
+        <div class="col-6" id="form-container">
+          <div id="header">
+            <br />
+            <h1>Registro de usuario</h1>
+            <hr />
+          </div>
+          <form class="formulario">
+            <div class="col mb-3">
+              <labe class="form-label">Username:</labe>
+              <input type="text" class="form-control" placeholder="username" />
+            </div>
+            <div class="col mb-3">
+              <labe class="form-label">Correo:</labe>
+              <input type="text" class="form-control" placeholder="correo" />
+            </div>
+            <div class="col mb-3">
+              <labe class="form-label">Clave:</labe>
+              <input type="text" class="form-control" placeholder="clave" />
+            </div>
+            <div class="col mb-3">
+              <labe class="form-label">Repetir clave:</labe>
+              <input type="text" class="form-control" placeholder="repetir clave" />
+            </div>
+            <div class="d-flex justify-content-center ">
+              <input class="btn btn-primary" type="submit" value="Registrarse">
+            </div>
+          </form><br/>
+           <div class="d-flex justify-content-center" >
+          <p>¿Ya tienes una cuenta?</p>
+        </div>
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-link ">Inicia Sesion</button>
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,10 +46,29 @@
 /* eslint-disable */
 export default {
   name: "RegistrarUsuario",
-  
+  data() {
+    return {
+      usuario: {
+        idUsuario: "",
+        username: "",
+        correo: "",
+        clave: "",
+        sessions: {
+        },
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
-
+#register {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 70vh;
+}
+#form-container {
+  width: 500px;
+}
 </style>
