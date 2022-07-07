e4f9e2fdd09532191a39e31ba23e097a24ea1722
Mara Villagran <m.villagran02@ufromail.cl>
2022-07-06 21:33:44 -0400

    Se sube componente IniciarSesion

--- proyecto-especializacion/src/components/IniciarSesion.vue
+++ proyecto-especializacion/src/components/IniciarSesion.vue
@@ -1,6 +1,32 @@
 <!-- eslint-disable  -->
 <template>
-  <div class="hello">
+  <div class="container">
+    <div class="row justify-content-md-center" id="login">
+      <div class="col-8" id="form-container">
+        <div class="header">
+          <h1>Iniciar Sesión</h1>
+          <p>Completa tus datos para iniciar sesión</p>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Username:</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="username">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Password:</label>
+          <input type="password" class="form-control" >
+        </div>
+        <div class="d-flex justify-content-center">
+          <input class="btn btn-primary" type="submit" value="Iniciar Sesión">
+        </div><br>
+        <div class="d-flex justify-content-center" >
+          <p>¿No tienes una cuenta?</p>
+        </div>
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-link ">Regístrate</button>
+        </div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +40,17 @@ export default {
 </script>
 
 <style scoped>
+#login{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+}
+#form-container{
+  width: 500px;
+}
+.header{
+  text-align: center;
+}
 
 </style>
